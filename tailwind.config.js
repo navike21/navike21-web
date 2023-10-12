fea1f72e0ca39fb2f4f5a9647e26e13fe845e730
José Iván Chapoñán Casiano <ivan@navike21.com>
2023-10-12 03:33:56 -0500

    feat: configuring colors to use on the web page

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,28 @@
 module.exports = {
 	content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
 	theme: {
+		colors: {
+			'yellow': {
+				'100': '#FFEB92',
+				'200': '#FFE46D',
+				'300': '#FFDE49',
+				'400': '#FFD724',
+				DEFAULT: '#FFD000',
+				'600': '#FFD000',
+				'700': '#FFC300',
+				'800': '#FFB700',
+				'900': '#FA0',
+			},
+			'black': {
+				'100': '#7C8EB7',
+				'200': '#556A9A',
+				'300': '#3D4C6E',
+				'400': '#252E43',
+				DEFAULT: '#0C0F16',
+				'600': '#0C0F15',
+				'700': '#0B0E14',
+			},
+		},
 		extend: {},
 		screens: {
 			xs: '300px',
