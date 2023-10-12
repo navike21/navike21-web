be72ace20680c27809e0c7e39d8227ada2252142
José Iván Chapoñán Casiano <ivan@navike21.com>
2023-10-12 03:03:12 -0500

    feat: Main web typography configuration

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,12 @@
 import './globals.css'
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import { Poppins } from 'next/font/google'
 import { ReactNode } from 'react'
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Poppins({
+	subsets: ['latin-ext'],
+	weight: ['300', '400', '500', '600', '700']
+})
 
 export const metadata: Metadata = {
 	title: 'Create Next App',
