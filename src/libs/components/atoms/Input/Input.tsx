0dbf7b3081183510c0a6caf56e0ccd34f6216fd1
José Iván Chapoñán Casiano <ivan@navike21.com>
2023-12-04 13:42:30 -0500

    feat: add unit test to input component

--- src/libs/components/atoms/Input/Input.tsx
+++ src/libs/components/atoms/Input/Input.tsx
@@ -1,37 +1,42 @@
 import clsx from 'clsx'
-import { useId, InputHTMLAttributes } from 'react'
+import { useId, InputHTMLAttributes, forwardRef } from 'react'
 
 interface IInputProps extends InputHTMLAttributes<HTMLInputElement> {
   label?: string
 }
 
-export const Input = ({ label, ...props }: IInputProps) => {
-  const idInput = useId()
+export const Input = forwardRef<HTMLInputElement, IInputProps>(
+  ({ label, ...props }, ref) => {
+    const idInput = useId()
 
-  return (
-    <label
-      htmlFor={idInput}
-      className={clsx(
-        'relative rounded-md ring-gray-600 dark:ring-white ring-inset ring-1 p-4 focus-within:ring-2 transition-all duration-500 ease-in-out flex'
-      )}
-    >
-      <input
-        type="text"
-        id={idInput}
-        className="peer border-none bg-transparent__white-0 placeholder-transparent focus:border-transparent focus:outline-none focus:ring-0 text-md dark:text-white w-full"
-        placeholder=""
-        {...props}
-      />
+    return (
+      <label
+        htmlFor={idInput}
+        className={clsx(
+          'relative rounded-md ring-gray-600 dark:ring-white ring-inset ring-1 p-4 focus-within:ring-2 transition-all duration-500 ease-in-out flex'
+        )}
+      >
+        <input
+          type="text"
+          id={idInput}
+          className="peer border-none bg-transparent__white-0 placeholder-transparent focus:border-transparent focus:outline-none focus:ring-0 text-md dark:text-white w-full"
+          placeholder=""
+          ref={ref}
+          {...props}
+        />
 
-      {label && (
-        <span
-          className={clsx(
-            'pointer-events-none absolute start-2.5 right-2 top-0 -translate-y-1/2 bg-white dark:bg-gray-900 dark:text-white p-1.5 text-xs text-gray-600 font-bold transition-all duration-500 peer-placeholder-shown:top-1/2 peer-placeholder-shown:text-sm peer-focus:top-0 peer-focus:text-xs peer-focus:right-auto'
-          )}
-        >
-          {label}
-        </span>
-      )}
-    </label>
-  )
-}
+        {label && (
+          <span
+            className={clsx(
+              'pointer-events-none absolute start-2.5 right-2 top-0 -translate-y-1/2 bg-white dark:bg-gray-900 dark:text-white p-1.5 text-xs text-gray-600 font-bold transition-all duration-500 peer-placeholder-shown:top-1/2 peer-placeholder-shown:text-sm peer-focus:top-0 peer-focus:text-xs peer-focus:right-auto'
+            )}
+          >
+            {label}
+          </span>
+        )}
+      </label>
+    )
+  }
+)
+
+Input.displayName = 'Input'
---
+++ src/libs/components/atoms/Input/input.test.tsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import { render } from '@testing-library/react'
+import { Input } from './Input'
+
+describe('Input Component', () => {
+  test('should render input component with label', () => {
+    const { getByLabelText } = render(<Input label="Label" />)
+    expect(getByLabelText('Label')).toBeTruthy()
+  })
+
+  test('should render input component without label', () => {
+    const { getByPlaceholderText } = render(<Input />)
+    expect(getByPlaceholderText('')).toBeTruthy()
+  })
+
+  test('should render input component with label and placeholder', () => {
+    const { getByPlaceholderText, getByLabelText } = render(
+      <Input label="Label" placeholder="Placeholder" />
+    )
+    expect(getByPlaceholderText('Placeholder')).toBeTruthy()
+    expect(getByLabelText('Label')).toBeTruthy()
+  })
+
+  test('should render input component with label and value', () => {
+    const { getByDisplayValue, getByLabelText } = render(
+      <Input label="Label" value="Value" />
+    )
+    expect(getByDisplayValue('Value')).toBeTruthy()
+    expect(getByLabelText('Label')).toBeTruthy()
+  })
+
+  test('should render input component with label and disabled', () => {
+    const { getByLabelText } = render(<Input label="Label" disabled />)
+    expect(getByLabelText('Label')).toBeTruthy()
+  })
+})
