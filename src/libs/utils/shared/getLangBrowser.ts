790b038f410cc3827bbbcacc24ec0b4403dfc680
José Iván Chapoñán Casiano <ivan@navike21.com>
2024-01-18 18:19:07 -0500

    feat: refactor getLangBrowser to handle cases where navigator is undefined

--- src/libs/utils/shared/getLangBrowser.ts
+++ src/libs/utils/shared/getLangBrowser.ts
@@ -1,4 +1,6 @@
 import { TLangSupported } from '@Types/shared'
 
 export const getLangBrowser = (): TLangSupported =>
-  navigator.language?.split('-')[0] as TLangSupported
+  typeof navigator !== 'undefined'
+    ? (navigator.language?.split('-')[0] as TLangSupported)
+    : 'en'
