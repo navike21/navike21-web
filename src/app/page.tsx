fd4e61357eaad714a3f0d974a053b42e80ee84e7
José Iván Chapoñán Casiano <ivan@navike21.com>
2024-02-18 01:27:20 -0500

    feat: add infoProject

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,3 @@
-import { Button } from '@Components/molecules'
-
 export default function Home() {
-  return (
-    <main>
-      <Button>Hola mundo</Button>
-      Hola
-    </main>
-  )
+  return <main>Hola</main>
 }
---
+++ src/libs/constants/shared/infoProject.ts
@@ -0,0 +1,11 @@
+export const COMPANY_NAME = 'navike21'
+export const COMPANY_URL = 'https://navike21.com'
+export const COMPANY_EMAIL = ['contacto@navike21.com']
+export const COMPANY_PHONE = ['+51 989 505 027', '+51 989 505 023']
+
+// RSS
+export const COMPANY_FACEBOOK = 'https://www.facebook.com/navike21'
+export const COMPANY_TWITTER = 'https://twitter.com/navike21'
+export const COMPANY_INSTAGRAM = 'https://www.instagram.com/navike21/'
+export const COMPANY_LINKEDIN = 'https://www.linkedin.com/company/navike21'
+export const COMPANY_GITHUB = 'https://github.com/navike21'
