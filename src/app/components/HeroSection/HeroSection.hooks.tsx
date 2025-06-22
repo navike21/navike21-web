05734225abbe152d86466c1f66d2ffebcf8d60b3
José Iván Chapoñán Casiano <ivan@navike21.com>
2025-06-22 01:23:38 -0500

    refactor(HeroSection): remove unused infoSlider data and related imports

--- src/app/components/HeroSection/HeroSection.hooks.tsx
+++ src/app/components/HeroSection/HeroSection.hooks.tsx
@@ -1,42 +1,9 @@
 import { ArrowButton } from '@Components/atoms'
-import {
-  manCheckingHisEmailLaptop,
-  supermarketWorkerSupplyingFruitDepartmentWithFood,
-  viewManWorkingDesk,
-  youngAttractiveWoman
-} from '@Constants/backgroundsImages'
 import clsx from 'clsx'
 import { ButtonHTMLAttributes } from 'react'
 import { Settings } from 'react-slick'
 
 export const useHeroSection = () => {
-  const infoSlider = [
-    {
-      title: 'Software a medida, **diseñado para el éxito.**',
-      description:
-        'Creamos soluciones digitales que impulsan tu negocio, con tecnología de vanguardia',
-      image: youngAttractiveWoman.src
-    },
-    {
-      title: 'Diseño que enamora, **experiencia que convierte.**',
-      description:
-        'Transformamos ideas en interfaces intuitivas, modernas y centradas en el usuario.',
-      image: viewManWorkingDesk.src
-    },
-    {
-      title: 'Vende más, **vende mejor.**',
-      description:
-        'Creamos tiendas online escalables y optimizadas para convertir visitantes en clientes.',
-      image: supermarketWorkerSupplyingFruitDepartmentWithFood.src
-    },
-    {
-      title: 'Conecta con tu audiencia, **sin esfuerzo.**',
-      description:
-        'Automatiza, segmenta y comunica con campañas de email que funcionan.',
-      image: manCheckingHisEmailLaptop.src
-    }
-  ]
-
   function ArrowSliderRight({
     onClick
   }: Readonly<ButtonHTMLAttributes<HTMLButtonElement>>) {
@@ -76,7 +43,6 @@ export const useHeroSection = () => {
   }
 
   return {
-    infoSlider,
     settingsSlider
   }
 }
