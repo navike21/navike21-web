733e29c513fa5c0d644ab3ec02fa4bd43a776978
José Iván Chapoñán Casiano <ivan@navike21.com>
2025-07-06 14:28:05 -0500

    Refactor code structure for improved readability and maintainability

--- src/app/components/RecentWorks/RecentWorks.tsx
+++ src/app/components/RecentWorks/RecentWorks.tsx
@@ -1,9 +1,13 @@
-import { Content } from '@Components/atoms'
+import { Content, LinkButton } from '@Components/atoms'
 import { Title } from '@Components/molecules'
 import clsx from 'clsx'
-import React from 'react'
+
+import iphone from '@Assets/images/devices/iphone.svg'
+import Image from 'next/image'
+import { clients } from '@Constants/clients'
 
 export const RecentWorks = () => {
+  const { images: { mobile } = {} } = clients[0]
   return (
     <div className="bg-slate-950">
       <Content
@@ -13,7 +17,7 @@ export const RecentWorks = () => {
         )}
       >
         <section
-          className={clsx('flex flex-col gap-5 items-center', 'md:w-5/12')}
+          className={clsx('flex flex-col gap-5 items-start', 'md:w-5/12')}
         >
           <Title
             subtitle="Trabajos recientes"
@@ -24,6 +28,12 @@ export const RecentWorks = () => {
             hemos aplicado nuestras habilidades y conocimientos para crear
             soluciones efectivas y atractivas.
           </p>
+          <LinkButton
+            href="/nosotros"
+            className={clsx('mt-4 w-full', 'sm:w-fit')}
+          >
+            Ver más proyectos
+          </LinkButton>
         </section>
         <div
           className={clsx(
@@ -34,13 +44,30 @@ export const RecentWorks = () => {
         />
         <section
           className={clsx(
-            'grid grid-cols-2 gap-3',
-            'sm:grid-cols-4 sm:gap-6',
-            'md:w-6/12 md:gap-4',
-            'lg:grid-cols-4 lg:gap-4 lg:w-6/12'
+            'relative flex justify-center items-center',
+            'before:inset-0 before:bg-[radial-gradient(circle,rgba(56,189,248,1)_0%,rgba(56,189,248,0)_80%)] before:rounded-3xl before:blur-3xl before:z-0',
+            'before:absolute before:top-0 before:left-0 before:right-2/12 before:bottom-2/12',
+            'after:inset-0 after:bg-[radial-gradient(circle,rgba(130,0,219,1)_0%,rgba(130,0,219,0)_80%)] after:rounded-3xl after:blur-3xl after:z-0',
+            'after:absolute after:top-2/12 after:left-2/12 after:right-0 after:bottom-0'
           )}
         >
-          s
+          <div className="w-8/12 relative z-10">
+            <Image
+              src={iphone}
+              alt="iPhone 12"
+              className="w-full relative z-10"
+            />
+            <div className="absolute top-[1%] bottom-2 left-0 right-0 m-auto w-11/12 h-auto bg-white pt-[13%] rounded-3xl overflow-hidden">
+              <Image
+                src={mobile?.[0].src ?? ''}
+                width={mobile?.[0].width}
+                height={mobile?.[0].height}
+                alt="iPhone 12"
+                className="w-full h-full object-cover z-0 object-left-top"
+                quality={100}
+              />
+            </div>
+          </div>
         </section>
       </Content>
     </div>
--- src/libs/constants/clients.ts
+++ src/libs/constants/clients.ts
@@ -11,13 +11,21 @@ import {
   TentacionesGourmet
 } from '@Components/atoms'
 import { ISvgProps } from '@Types/svg'
+import { StaticImageData } from 'next/image'
 import { ReactNode } from 'react'
 
+import homeTentaciones from '@Assets/images/projects/tentaciones-gourmet/converted/home-mobile-thumb.webp'
+
+interface IClientImage {
+  mobile: StaticImageData[]
+}
 interface IClient {
   id: string
   name: string
   best?: boolean
+  url: string
   logo: (params: ISvgProps) => ReactNode
+  images?: IClientImage
 }
 
 export const clients: IClient[] = [
@@ -25,51 +33,64 @@ export const clients: IClient[] = [
     id: 'tentaciones-gourmet',
     name: 'Tentaciones Gourmet',
     best: true,
-    logo: TentacionesGourmet
+    url: 'tentaciones-gourmet.pe',
+    logo: TentacionesGourmet,
+    images: {
+      mobile: [homeTentaciones]
+    }
   },
   {
     id: 'almazen',
     name: 'Almazen',
+    url: 'almazen.com.pe',
     logo: Almazen
   },
   {
     id: 'anker',
     name: 'Anker',
+    url: 'anker-paints.com',
     logo: Anker
   },
   {
     id: 'beats',
     name: 'Beats',
+    url: 'beatsmusica.com',
     logo: Beats
   },
   {
     id: 'carbyne',
     name: 'Carbyne',
+    url: 'carbyne-safety.com',
     logo: Carbyne
   },
   {
     id: 'circurela',
     name: 'Circurela',
+    url: 'circurela.com',
     logo: Circurela
   },
   {
     id: 'colegio-la-union',
     name: 'Colegio La Unión',
+    url: 'www.launion.edu.pe',
     logo: ColegioLaUnion
   },
   {
     id: 'eurogourmet',
     name: 'Eurogourmet',
+    url: 'eurogourmet.com.pe',
     logo: EuroGourmet
   },
   {
     id: 'hammer-blocs',
     name: 'Hammer Blocs',
+    url: 'hammerblocs.com',
     logo: HammerBlocs
   },
   {
     id: 'rk-power',
     name: 'RK Power',
+    url: 'rkpower.com',
     logo: RkPower
   }
 ]
