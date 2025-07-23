d7b1abcbc7c4b42d17c19839cd657ae46965db28
José Iván Chapoñán Casiano <ivan@navike21.com>
2025-07-23 08:21:21 -0500

    feat: implement dynamic page rendering with language support in Pages component

--- src/app/[lang]/[pages]/page.tsx
+++ src/app/[lang]/[pages]/page.tsx
@@ -1,3 +1,35 @@
-export default function Pages() {
-  return <div>Pages Component</div>
+import { notFound, redirect } from 'next/navigation'
+import { pages } from '@Translations/pages'
+import { TLanguageKey } from '@Types/languages'
+import { HOME } from '@Constants/pages'
+
+interface IParams {
+  params: Promise<{
+    lang: TLanguageKey
+    pages: string
+  }>
+}
+
+export default async function Pages({ params }: Readonly<IParams>) {
+  const { lang, pages: slug } = await params
+
+  const matchedPage = pages.find(
+    ({ language }) => language[lang]?.slug === slug
+  )
+
+  if (!matchedPage) {
+    notFound()
+  }
+
+  if (matchedPage.id === HOME) {
+    redirect(`/${lang}/`)
+  }
+
+  const { id } = matchedPage
+
+  return (
+    <div>
+      {matchedPage.language[lang].title} {id}
+    </div>
+  )
 }
