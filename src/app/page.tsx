3100036420b221a421105e477be1aafb20083c5b
José Iván Chapoñán Casiano <ivan@navike21.com>
2025-07-23 07:26:17 -0500

    feat: implement language detection in RootPage for dynamic redirection based on browser language

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,21 @@
+import { headers } from 'next/headers'
 import { redirect } from 'next/navigation'
+import { ES, SUPPORTED_LANGUAGES } from '@Constants/languages'
+import { TLanguageKey } from '@Types/languages'
 
-export default function RootPage() {
-  redirect('/es/')
+export default async function RootPage() {
+  const headersList = await headers()
+  const acceptLanguage = headersList.get('accept-language')
+
+  const browserLang = acceptLanguage?.split(',')[0].split('-')[0] ?? ES
+
+  const supportedKeys = Object.keys(SUPPORTED_LANGUAGES) as TLanguageKey[]
+
+  const langToUse: TLanguageKey = supportedKeys.includes(
+    browserLang as TLanguageKey
+  )
+    ? (browserLang as TLanguageKey)
+    : ES
+
+  redirect(`/${langToUse}`)
 }
