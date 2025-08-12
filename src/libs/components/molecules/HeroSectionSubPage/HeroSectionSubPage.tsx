f9668175b16f45d8d2d1526184558367cd6a1fae
José Iván Chapoñán Casiano <ivan@navike21.com>
2025-08-12 06:22:16 -0500

    fix: update text color in HeroSectionSubPage and enhance scrollbar styles in globals.css

--- src/libs/components/molecules/HeroSectionSubPage/HeroSectionSubPage.tsx
+++ src/libs/components/molecules/HeroSectionSubPage/HeroSectionSubPage.tsx
@@ -29,7 +29,7 @@ export const HeroSectionSubPage = ({
       <div
         className={clsx(
           'absolute left-0 right-0 top-0 -bottom-7 z-10',
-          'bg-gradient-to-b from-slate-950/0 via-slate-950/0 to-slate-950'
+          'bg-gradient-to-b from-slate-950/0 via-slate-950/90 via-80% to-slate-950'
         )}
       />
       <BackgroundParallax
