b7c53742bb35c8abd30f5f3bc5a13f2aae423b68
José Iván Chapoñán Casiano <ivan@navike21.com>
2025-11-01 05:35:15 -0500

    feat: implement Testimonials component; refactor Testimonial structure and update exports

--- src/libs/components/molecules/Testimonials/Testimonials.tsx
+++ src/libs/components/molecules/Testimonials/Testimonials.tsx
@@ -1,6 +1,4 @@
 import { Avatar, Card, Divider, IconComponent } from '@Components/atoms'
-import React from 'react'
-
 import imageTest from '@Assets/images/technology-expert-developing-userfriendly-software-by-understanding-requirements-endusers.jpg'
 import { type StaticImageData } from 'next/image'
 import clsx from 'clsx'
@@ -8,7 +6,7 @@ import { Slider } from '../Slider'
 import { uuid } from '@Helpers/uuid'
 import type { SplideProps } from '@splidejs/react-splide'
 
-interface ITestimonialItemProps {
+interface ITestimonialsItemProps {
   author: string
   role: string
   content: string
@@ -16,13 +14,13 @@ interface ITestimonialItemProps {
   starRating?: number
 }
 
-export const TestimonialItem = ({
+export const TestimonialsItem = ({
   author,
   role,
   content,
   avatar,
   starRating
-}: ITestimonialItemProps) => (
+}: ITestimonialsItemProps) => (
   <div className="w-full py-5 px-3 flex flex-col items-center">
     <Card className="max-w-80 w-[98%] h-full">
       <div className="flex flex-col gap-6">
@@ -57,7 +55,7 @@ export const TestimonialItem = ({
   </div>
 )
 
-export const Testimonial = () => {
+export const Testimonials = () => {
   const OPTIONS: SplideProps['options'] = {
     perPage: 4,
     width: '100%',
@@ -79,35 +77,35 @@ export const Testimonial = () => {
   return (
     <div className="flex items-center justify-center gap-6 flex-wrap">
       <Slider options={OPTIONS}>
-        <TestimonialItem
+        <TestimonialsItem
           author="John Doe"
           role="CEO, Company Inc."
           content="navike21 transformó nuestra presencia en línea. Su equipo es profesional y dedicado."
           avatar={imageTest}
           starRating={5}
         />
-        <TestimonialItem
+        <TestimonialsItem
           author="Jane Smith"
           role="Marketing Manager, Business Co."
           content="Gracias a navike21, nuestras ventas en línea se dispararon. ¡Altamente recomendado!"
           avatar={imageTest}
           starRating={4}
         />
-        <TestimonialItem
+        <TestimonialsItem
           author="Alice Johnson"
           role="Freelancer"
           content="El equipo de navike21 es increíblemente talentoso y fácil de trabajar. ¡Mi sitio web nunca se vio mejor!"
           avatar={imageTest}
           starRating={5}
         />
-        <TestimonialItem
+        <TestimonialsItem
           author="Bob Brown"
           role="Entrepreneur"
           content="navike21 superó mis expectativas en cada paso del camino. Su atención al detalle es insuperable."
           avatar={imageTest}
           starRating={5}
         />
-        <TestimonialItem
+        <TestimonialsItem
           author="Emily Davis"
           role="Small Business Owner"
           content="Estoy encantada con el trabajo de navike21. Mi negocio ha crecido gracias a su experiencia en desarrollo web."
