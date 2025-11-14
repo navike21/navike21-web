811dbc7f9beb26d4c507ab03844d5a753de68d19
José Iván Chapoñán Casiano <ivan@navike21.com>
2025-11-14 10:34:41 -0500

    feat: refactor Clients component layout and styling for improved responsiveness and readability

--- src/libs/components/molecules/Clients/Clients.tsx
+++ src/libs/components/molecules/Clients/Clients.tsx
@@ -5,13 +5,37 @@ interface IClientsProps {}
 
 export const Clients = ({}: IClientsProps) => {
   return (
-    <div className={clsx('clients-area flex flex-col items-center gap-10')}>
-      <section>¿Te sumas? Así como ellos que confían en nosotros</section>
+    <div
+      className={clsx(
+        'clients-area flex flex-col items-center gap-10',
+        'md:flex-row md:gap-20 md:justify-center'
+      )}
+    >
+      <section>
+        <h2
+          className={clsx(
+            'text-center text-3xl font-semibold w-full',
+            'md:text-left',
+            'lg:text-4xl'
+          )}
+        >
+          ¿Te sumas?
+        </h2>
+        <h2
+          className={clsx(
+            'text-center text-xl font-medium w-full',
+            'md:text-left',
+            'lg:text-2xl'
+          )}
+        >
+          Así como ellos que confían en nosotros
+        </h2>
+      </section>
       <section
         className={clsx(
           'grid grid-cols-2 gap-6 w-full',
           'sm:grid-cols-3',
-          'md:grid-cols-4',
+          'md:grid-cols-5',
           'lg:grid-cols-6'
         )}
       >
@@ -19,7 +43,10 @@ export const Clients = ({}: IClientsProps) => {
           return (
             <div
               key={id}
-              className="max-w-32 w-full aspect-square col-span-1 p-3"
+              className={clsx(
+                'max-w-32 w-full aspect-square col-span-1 m-auto',
+                'lg:max-w-28'
+              )}
             >
               <a
                 href={url}
